fd83142b14d3f001d4080d2b0b4847f287225e2a
Ash_Crow <sylvain@ashtree.eu>
2018-03-06 18:22:03 +0100

    Third pass: full catalog

--- an-persons.py
+++ an-persons.py
@@ -2,29 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import csv
-import re
+
 csv_input = "resources/Datathon-AN-10mars2018/persons.csv"
-csv_query = "resources/Datathon-AN-10mars2018/query.csv"
+csv_output = "resources/Datathon-AN-10mars2018/persons_output.csv"
 
-isni_ids = {}
-with open(csv_query, 'r') as csv_query_file:
-    reader = csv.DictReader(csv_query_file, delimiter=',')
-    for line in reader:
-        print(line)
-        isni_id = line['isni']
-        isni_ids[isni_id] = {
-            'qid': line['human'].split('/')[-1],
-            'name': line['humanLabel']
-        }
+with open(csv_output, 'w') as csv_output_file:
+    fieldnames = [
+        'id',
+        'label',
+        'description']
+    writer = csv.DictWriter(csv_output_file, fieldnames=fieldnames)
+    writer.writeheader()
 
+    with open(csv_input, 'r') as csv_input_file:
+        reader = csv.DictReader(csv_input_file, delimiter='\t')
+        for line in reader:
+            newline = {}
+            newline['id'] = line['recordId']
+            newline['description'] = line['description_or_functions']
+            name_parts = line['standardizedNameForm'].split('(')[0].split(', ')
+            if len(name_parts) > 1:
+                newline['label'] = "{} {}".format(
+                    name_parts[1].strip(),
+                    name_parts[0].strip()
+                )
+            else:
+                newline['label'] = name_parts[0]
 
-with open(csv_input, 'r') as csv_input_file:
-    reader = csv.DictReader(csv_input_file, delimiter='\t')
-    for line in reader:
-        if line['isni']:
-            source_isni_id = line['isni'].strip()
-            if source_isni_id in isni_ids:
-                print('{}\tP3599\t"{}"'.format(
-                    isni_ids[source_isni_id]['qid'],
-                    line['recordId']
-                ))
+            writer.writerow(newline)
